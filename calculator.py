01ee1b9cd8e6f0dd9200ede536469b1e8ba7ce99
Ayushya Singh <112999122+ayushyasingh@users.noreply.github.com>
2022-12-08 16:46:02 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# Calculator using Python
+print("***** Simple/Basic Calculator *****\nby Ayushya Singh")
+print("\n")
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+print("\n")
+print("Addition of", a, "+", b, "is", int(a) + int(b))
+print("Subtraction of", a, "-", b, "is", int(a) - int(b))
+print("Multiplication of", a, "*", b, "is", int(a) * int(b))
+print("Divison of", a, "/", b, "is", int(a) / int(b))
+print("Exponential of", a, "**", b, "is", int(a)**int(b))
+print("\n")
+print(
+  "You can Follow me on Twitter/Facebook/Instagram with username- @ayushyakmrsingh and on GitHub- @ayushyasingh"
+)
